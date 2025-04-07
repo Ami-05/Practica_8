c33d982354ccc57c27a2602916e941284d4cd4bc
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-04-06 19:25:52 -0600

    Ejercicio_2.c

---
+++ Ejercicio_2.c
@@ -0,0 +1,11 @@
+//Cruz Apodaca Amiel Merari//
+//Este programa muestra en pantalla los numeros enteros del 1 al 100//
+
+#include <stdio.h>
+
+int main() {
+  for (int i = 1; i<= 100; i++) {
+     printf("%d\n", i);
+    }
+    return 0;
+}
